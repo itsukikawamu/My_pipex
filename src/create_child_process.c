8c1a5b898209a0d35eee4255203e78063e5dd87c
ikawamuk <ikawamuk@student.42.fr>
2025-07-07 00:07:37 +0900

    update:

--- src/create_child_process.c
+++ src/create_child_process.c
@@ -6,14 +6,13 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 18:59:10 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/06 23:57:11 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/07 00:07:22 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "utils.h"
 
-
-int create_child_process(int **pipefd, t_ctx *ctx)
+int f(int **pipefd, t_ctx *ctx)
 {
 	pid_t	pid;
 
--- src/pipex_core.c
+++ src/pipex_core.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 18:51:22 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/06 23:30:00 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/07 00:06:56 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,9 @@ int	pipex_core(t_ctx *ctx)
 	
 	while (i < ctx->cmd_num)
 	{
-		// set_fd
-		if (create_child_process(&pipefd, ctx) == -1)
+		// if (set_fd() == -1)
+		// 	return (-1);
+		if (f(&pipefd, ctx) == -1)
 			return (-1);
 		ctx->cmd++;
 		i++;
