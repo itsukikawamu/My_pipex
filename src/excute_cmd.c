3f745044898a602a066a0202307c83dfb28d737d
ikawamuk <ikawamuk@student.42.fr>
2025-07-11 21:40:54 +0900

    update:

--- src/excute_cmd.c
+++ src/excute_cmd.c
@@ -6,7 +6,7 @@
 /*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 18:59:10 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/07/11 21:37:15 by ikawamuk         ###   ########.fr       */
+/*   Updated: 2025/07/11 21:40:52 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	excute(t_cp *cp)
 {
 	if (find_exec_file(cp) == -1)
 		return (-1);
-	if (connect_child_process() == -1)
+	if (connect_child_process(cp) == -1)
 		return (-1);
 	if (execve(cp->cmd_path, cp->cmd, cp->ep) == -1)
 		return (-1);
